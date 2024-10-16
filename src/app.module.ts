695ff2ac5990e9874522a0aa6565b0be7981b50f
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-16 11:38:11 +0530

    Changes to backend api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 import { EpisodesModule } from './episodes/episodes.module';
 import { TopicsModule } from './topics/topics.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [EpisodesModule, TopicsModule, ConfigModule],
