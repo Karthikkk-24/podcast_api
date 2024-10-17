57b1445750b034c6ec99a5d95cf33a32c42f305d
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-17 09:18:32 +0530

    Modified Episode Controller Class

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,25 +1,29 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ConfigService } from 'src/config/config.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
 import { EpisodesService } from './episodes.service';
 
 @Controller('episodes')
 export class EpisodesController {
-  constructor(private episodesService: EpisodesService) {}
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
     console.log(sort);
-    return 'all episodes';
+    return this.episodesService.findAll();
   }
 
   @Get('featured')
   findFeatured() {
-    return 'featured episodes';
+    return this.episodesService.findFeatured();
   }
 
   @Get(':id')
   findOne(@Param() id: string) {
-    return 'one episode';
+    return this.episodesService.findOne(id);
   }
 
   @Post()
