c46d0f08fb16ff84c45d0a2e5ca254203729bea4
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-17 09:24:51 +0530

    Modified Episodes Contoller Test File

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from 'src/config/config.module';
 import { EpisodesController } from './episodes.controller';
+import { EpisodesService } from './episodes.service';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
+  const mockEpisodesService = {
+    findAll: async () => [{ id: 'id' }],
+    findFeaturedEpisodes: async () => [{ id: 'id' }],
+    findOne: async () => [{ id: 'id' }],
+    create: async () => [{ id: 'id' }],
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [EpisodesController],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService }],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
